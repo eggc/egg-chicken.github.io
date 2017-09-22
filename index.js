25bdc991bfb3868a41e5c8015238c95fa81a3205
Ken Eguchi <eguchi@haw.co.jp>
2017-09-23 08:48:41 +0900

    目次を追加

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const container = document.querySelector('#tech');
 const url = 'https://egg-chicken.github.io/tech/github_pages.md';
 fetch(url)
   .then(response => response.text())
-  .then(text => container.innerHTML = marked(text));
+  .then(text => {
+    container.innerHTML = marked(text);
+    tocbot.init({headingSelector: 'h1'});
+  });
