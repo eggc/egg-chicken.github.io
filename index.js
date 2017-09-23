d4664f0a368ba73d90917a3e58209dc2ceda01ed
Ken Eguchi <eguchi@haw.co.jp>
2017-09-23 09:43:01 +0900

    手続き的なプログラムをオブジェクト指向に書き換え

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const container = document.querySelector('#tech');
-const url = 'https://egg-chicken.github.io/tech/github_pages.md';
-fetch(url)
-  .then(response => response.text())
-  .then(text => {
-    container.innerHTML = marked(text);
-    tocbot.init({headingSelector: 'h1'});
-  });
+const Lib = {marked, tocbot};
+class Section {
+  constructor(selector) {
+    this.element = document.createElement('section');
+    document.querySelector(selector).appendChild(this.element);
+  }
+
+  fetch(url) {
+    return fetch(url)
+      .then(response => response.text())
+      .then(text => this.element.innerHTML = Lib.marked(text));
+  }
+}
+
+class App {
+  static start() {
+    (new App).start();
+  }
+
+  toc() {
+    Lib.tocbot.init({headingSelector: 'h1'});
+  }
+
+  start() {
+    const section = new Section('#tech');
+    section.fetch('https://egg-chicken.github.io/tech/github_pages.md').then(this.toc);
+  }
+}
+
+App.start();
