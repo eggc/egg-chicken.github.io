7e16d7c8f3bdf634f366f2051100b518acdd6406
Ken Eguchi <no.eggchicken@gmail.com>
2018-11-04 00:56:49 +0900

    markdown をやめた

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-const Lib = {marked, tocbot}
+const Lib = {tocbot}
 
 class App {
   static start() {
-    var app = (new App)
-    app.markdown()
-    app.toc()
-  }
-
-  toc() {
-    Lib.tocbot.init({headingSelector: 'h1'})
-  }
-
-  markdown() {
-    var pre = document.querySelector('#source-code')
-    var main = document.querySelector('#tech')
-    var text = pre.innerText
-    main.innerHTML = Lib.marked(text)
+    Lib.tocbot.init();
   }
 }
 
